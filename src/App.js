ec57f123b3c3dba91d26f3de2f5b264e3a99f0a5
tjcheah <tatiana.cheah@gmail.com>
2021-10-13 10:22:05 +0800

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
+  const name = 'Tati';
+
+  const age = 24;
+
+  //age Calculation
+  const ageCalculation = (
+    <div>
+      <p>
+        {age > 20
+          ? 'Old. Both physically and in soul.'
+          : 'You a child'}
+      </p>
+    </div>
+  );
+
+  //Mapping
+  const data = ['bitcoin', 'ethereum', 'doge'];
+
+  const mapping = (
+    <h1>
+      <ul>
+        {data.map((crypto, idx) => {
+          return <li key={idx}> {crypto} </li>;
+        })}
+      </ul>
+    </h1>
+  );
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Hello {name}!</h1>
+      <p>Calculation: 1 + 3 = {1 + 3}</p>
+      {ageCalculation}
+      {mapping}
     </div>
   );
 }
